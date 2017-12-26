dc6b4a232305969a3c06b37f8c1120b7f72c0af1
Brian Lim <brian01.lim@gmail.com>
2017-12-25 18:31:29 -0800

    Refactored recordconvo code according to codeclimate

--- recordconvo.py
+++ recordconvo.py
@@ -12,50 +12,40 @@ edit_record = open('edited_recorded_messages.csv', 'a')
 edit_recorder = csv.writer(edit_record)
 print('opened edited_recorded_messages.csv to append')
 
-def format_record(time_str, message_id, user_id, user_name, message_content):
-	return [time_str, message_id, user_id, user_name, message_content]
+def format_record(time_str, message, message_content):
+	return [time_str, message.id, message.author.id, message.author.name, message_content]
 
 def record_init():
 	recorder.writerow(format_record(
 		datetime.now(), 
-		None, 
-		None, 
-		None, 
+		None,
 		"Recording started"
 	))
 
 def record_message(message):
 	recorder.writerow(format_record(
 		message.timestamp, 
-		message.id, 
-		message.author.id, 
-		message.author.name, 
+		message,
 		message.content
 	))
 
 def record_message_edit(message):
 	edit_recorder.writerow(format_record(
 		message.edited_timestamp,
-		message.id,
-		message.author.id,
-		message.author.name,
+		message,
 		message.content
 	))
 
 def record_message_delete(message):
 	edit_recorder.writerow(format_record(
 		datetime.now(),
-		message.id,
-		message.author.id,
-		message.author.name,
+		message,
 		"DELETED"
 	))
 
 def record_end():
 	recorder.writerow(format_record(
 		datetime.now(), 
-		None, 
-		None, 
-		None, 
+		None,
 		"Recording ended"
 	))
