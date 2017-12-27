6c31baa3727d9efe93b23ea7f0a529cbe4fd41f1
Brian Lim <brian01.lim@gmail.com>
2017-12-27 15:34:56 -0800

    Reverted those changes and made it less annoying

--- discordsim.py
+++ discordsim.py
@@ -2,19 +2,13 @@ import markovify
 from datetime import timedelta
 
 message_cache = 'cached_messages.txt'
-email_sim = 'emails.txt'
-email_model = markovify.Text(open('emails.txt').read())
-print('Markov Model loaded')
+
 SIMULATION_INTERVAL = timedelta(hours=1)
-def simulate(filename, test=True, nl=True):
+def simulate(filename):
 	with open(filename) as file:
-		if nl:
-			text = file.read()
-			markov_model = markovify.NewlineText(text)
-			simulation = markov_model.make_sentence(test_output=test)
-		else:
-			markov_model = email_model
-			simulation = markov_model.make_short_sentence(60)
+		text = file.read()
+		markov_model = markovify.NewlineText(text)
+		simulation = markov_model.make_sentence(tries=20, test_output=test)
 		if simulation is not None:
 			return clean_text(simulation)
 		else:
