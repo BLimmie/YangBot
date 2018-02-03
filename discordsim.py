ef0649b356beb8785e2a54127f70f48152d43f5f
Brian Lim <brian01.lim@gmail.com>
2018-02-02 22:39:46 -0800

    updated a small thing

--- discordsim.py
+++ discordsim.py
@@ -1,14 +1,14 @@
 import markovify
 from datetime import timedelta
 
-message_cache = 'cached_messages.txt'
-
+message_cache_ucsb = 'cached_messages_ucsb.txt'
+message_cache_full = 'cached_messages_uc.txt'
 SIMULATION_INTERVAL = timedelta(hours=1)
 def simulate(filename):
 	with open(filename) as file:
 		text = file.read()
 		markov_model = markovify.NewlineText(text)
-		simulation = markov_model.make_sentence(max_overlap_ratio = 0.4, max_overlap_total = 10)
+		simulation = markov_model.make_sentence(max_overlap_ratio = 0.4, max_overlap_total = 3)
 		if simulation is not None:
 			return clean_text(simulation)
 		else:
