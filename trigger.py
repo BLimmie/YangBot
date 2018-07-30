bd1efba8740775fb8c888da72c9741559a283bd0
itsme180 <41887385+itsme180@users.noreply.github.com>
2018-07-29 22:24:07 -0700

    Update trigger.py

--- trigger.py
+++ trigger.py
@@ -1,11 +1,8 @@
 #YangBot's responses
 choiced_responses = {
-	"dank": "I sure hope you're talking about dank memes and not that dank green!",
-	"blaze": "I sure hope you're talking about the pizza!",
 	"alcohol": "Reminder that underage drinking is prohibited at UCSB.", #should only be triggered by gauchito
 	"mj": "Despite the passing of Prop 64, marijuana usage is prohibited at UCSB except for university-approved research.",
 	"adderall": "If you need to study, go to the UCSB Library. Most people focus the best on the 8th floor.",
-	"fire": "Put on a mask and get back to studying",
 	"drug": "Substance abuse is prohibited at UCSB",
 	"party": "Go back to studying"
 }
@@ -13,13 +10,10 @@ choiced_responses = {
 
 #Words that trigger YangBot. These are directly connected to the responses above with the words on the left
 trigger_words = {
-	"dank": ['dank'],
-	"blaze": ['blaze'],
 	"alcohol": ['alcohol', 'vodka', 'wine', 'beer', 'drunk', 'whiskey', 'beers'], #should only be triggered by gauchito
 	"mj": ['mj', '420', 'weed', 'kush', 'marijuana'],
 	"adderall": ['adderall', 'adderal', 'addy'],
 	"drug": ['drug', 'acid', 'lsd', 'shrooms', 'xanax', 'coke', 'cocaine'],
-	"fire": ['fire'],
 	"party": ['party']
 }
 
