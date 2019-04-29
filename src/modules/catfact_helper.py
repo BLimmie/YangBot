ff9cf3ff186dad10eef5b0fb38fe80367fd13292
BLimmie <brian01.lim@gmail.com>
2019-04-29 12:59:28 -0700

    fixed asyncio bug

--- src/modules/catfact_helper.py
+++ src/modules/catfact_helper.py
@@ -7,7 +7,13 @@ def get_catfact():
             async with session.get('https://catfact.ninja/fact') as resp:
                 data = await resp.json()
                 return data['fact']
-    return asyncio.get_event_loop().run_until_complete(_get_catfact())
+    main_loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    result = loop.run_until_complete(_get_catfact())
+    loop.close()
+    asyncio.set_event_loop(main_loop)
+    return result
     
 
 
