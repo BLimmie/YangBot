4f642f41b96067188b489900a4456715c66ebfc7
BLimmie <brian01.lim@gmail.com>
2019-04-29 13:09:05 -0700

    format to have unsubscribe

--- src/modules/catfact_helper.py
+++ src/modules/catfact_helper.py
@@ -1,9 +1,18 @@
 import requests
 
+def send_format(catfact):
+    return """
+    Did you know:
+    ```
+    {}
+    ```
+    Type "UNSUBSCRIBE" to unsubscribe from future catfacts.
+    """.format(catfact)
+
 def get_catfact():
 	response = requests.get("https://catfact.ninja/fact")
 	data = response.json()
-	return data['fact']
+	return send_format(data['fact'])
     
 
 
