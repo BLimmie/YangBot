a06664723621c60623ef26ebf929f45d11b54580
BLimmie <brian01.lim@gmail.com>
2019-05-01 15:15:41 -0700

    toxicity restored

--- src/auto_on_message.py
+++ src/auto_on_message.py
@@ -3,7 +3,9 @@ import discord
 
 from src.tools.message_return import message_data
 from src.modules.catfact_helper import get_catfact
+import src.modules.toxicity_helper as toxicity_helper
 
+SUPER_TOXIC_THRESHOLD = .91
 
 def init(bot):
     
@@ -17,6 +19,16 @@ def init(bot):
         if isinstance(message.channel, (discord.DMChannel, discord.GroupChannel)):
             return message_data(message.channel, "I do not reply to private messages. If you have any questions, please message one of the mods.")
         return None
+
+    async def remove_toxicity(message, score, toxic_message):
+        if score > SUPER_TOXIC_THRESHOLD:
+            await toxic_message.delete()
+            await toxic_message.channel.send("We didn't accept you into this school to be toxic.")
+
+    @bot.auto_on_message(None, None, True)
+    def check_toxicity(message):
+        send_message, score = toxicity_helper.get_toxicity(message)
+        return message_data(bot.config["toxic_notif_channel"], send_message, args=[score,message])
     # @bot.auto_on_message(None,None,True)
     # def test(message):
     #     print(message.author.nick)
---
+++ src/modules/toxicity_helper.py
@@ -0,0 +1,47 @@
+import requests
+import os
+import json
+
+api_key = os.environ["PERSPECTIVE_API"]
+TOXIC_THRESHOLD = .83
+header = {'Content-type': 'application/json'}
+
+url = 'https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key={}'.format(api_key)
+
+def format_json(message):
+    data = {'comment': {'text': message}, 'languages': ["en"],'requestedAttributes': {'TOXICITY':{}, 'SEVERE_TOXICITY': {}} }
+    return json.dumps(data)
+
+def send_format(message, score):
+    return  "Message has been marked for toxicity:\nUser: {}\nChannel: {}\nTime: {}\nMessage: {}\nCertainty: {}".format(
+                    message.author.display_name, 
+                    message.channel.mention, 
+                    (message.timestamp - timedelta(hours=7)), 
+                    message.clean_content, 
+                    score*100
+                )
+
+def _get_toxicity(message_content):
+    toxic_json = requests.post(url, headers=header, data=format_json(message_content))
+    toxic_json = toxic_json.json()
+    try:
+        score = toxic_json['attributeScores']['SEVERE_TOXICITY']['summaryScore']['value']
+        return (score > TOXIC_THRESHOLD), score
+    except Exception as e:
+        print(toxic_json)
+        return False, score
+
+def get_toxicity(message):
+    """
+    Pass a message, not a string
+    """
+    is_toxic, score = _get_toxicity(message.clean_content)
+    if is_toxic:
+        return send_format(message, score), score
+    else:
+        return None
+    
+
+
+if __name__ == "__main__":
+    print(_get_toxicity("You're a fucking piece of shit"))
