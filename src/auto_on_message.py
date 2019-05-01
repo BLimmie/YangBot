e22f3aa812f158b1178b8c8706afe63f528030d1
BLimmie <brian01.lim@gmail.com>
2019-05-01 12:56:58 -0700

    reply to private

--- src/auto_on_message.py
+++ src/auto_on_message.py
@@ -1,15 +1,22 @@
 import psycopg2
+import discord
 
 from src.tools.message_return import message_data
 from src.modules.catfact_helper import get_catfact
 
 
 def init(bot):
-    pass
+    
     @bot.auto_on_message(None, None, True)
     def unsubscribe(message):
         if message.content.lower().strip() == "unsubscribe":
             return message_data(message.channel, get_catfact())
+
+    @bot.auto_on_message(None, None, True)
+    def private_message(message):
+        if isinstance(message.channel, (discord.DMChannel, discord.GroundChannel)):
+            return message_data(message.channel, "I do not reply to private messages. If you have any questions, please message one of the mods.")
+        return None
     # @bot.auto_on_message(None,None,True)
     # def test(message):
     #     print(message.author.nick)
