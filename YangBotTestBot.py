8a10f7145de3c43005d98dd25cbb9520c9d6109d
Dulaan <fu.dylan@gmail.com>
2021-07-01 13:17:48 -0700

    Update YangBotTestBot.py

--- YangBotTestBot.py
+++ YangBotTestBot.py
@@ -31,86 +31,6 @@ class catfact(integration_test):
         return 'test' in message.content
       return m
 
-class register_absent(integration_test):
-# registering to db w/o existing entry
-  def __init__(self):
-    integration_test.__init__(self)
-    self.message = '$register'
-  def check(self):
-    def m(message):
-      return 'User registered' in message.content
-    return m
-
-class register_present(integration_test):
-# registering to db w/ existing entry
-  def __init__(self):
-    integration_test.__init__(self)
-    self.message = '$register'
-  def check(self):
-    def m(message):
-      return 'User already registered' in message.content
-    return m
-
-class resetregister_present(integration_test):
-# resetting db entry w/ existing entry
-  def __init__(self):
-    integration_test.__init__(self)
-    self.message = '$resetregister'
-  def check(self):
-    def m(message):
-      return 'User registration reset' in message.content
-    return m
-
-class resetregister_absent(integration_test):
-# resetting db entry w/o existing entry
-  def __init__(self):
-    integration_test.__init__(self)
-    self.message = '$resetregister'
-  def check(self):
-    def m(message):
-      return 'User not registered. Use $register to register.' in message.content
-    return m
-
-class kickme(integration_test):
-# test kick command
-  def __init__(self):
-    integration_test.__init__(self)
-    self.message = '$kickme'
-  def check(self):
-    def m(message):
-      return 'you' in message.content.lower()
-    return m
-
-class nickname_empty(integration_test):
-# nickname command w/ missing field
-  def __init__(self):
-    integration_test.__init__(self)
-    self.message = '$nickname'
-  def check(self):
-    def m(message):
-      return 'No nickname requested' in message.content
-    return m
-
-class nickname_too_long(integration_test):
-# nickname command with too long nickname
-  def __init__(self):
-    integration_test.__init__(self)
-    self.message = '$nickname qwertyuiopasdfghjklzxcvbnmqwertyu'
-  def check(self):
-    def m(message):
-      return 'Nickname requested is too long' in message.content
-    return m
-
-class send(integration_test):
-# testing send command
-  def __init__(self):
-    integration_test.__init__(self)
-    self.message = '$send #dev-testing hello'
-  def check(self):
-    def m(message):
-      return 'hello' in message.content
-    return m
-
 class choose_empty(integration_test):
 # choose command unfilled
   def __init__(self):
@@ -128,10 +48,20 @@ class choose(integration_test):
     self.message = '$choose x;x'
   def check(self):
     def m(message):
-      return 'x' in message.description
+      return 'x' in message.description # check this syntax for embeds
     return m
 
+class toxicity_check(integration_test):
+  def __init__(self):
+    integration_test.__init__(self)
+    self.message = 'hey moron'
+def check(self):
+  def m(message):
+    return "Message has been marked for toxicity:" in message.embed.title and message.channel == 421899094357704704
+  return m 
+
 @bot.command(name = 'test')
+# command to run all integration tests
 async def test(m):
   testout = '```\nTest Results:\n'
   await m.send('testing...')
