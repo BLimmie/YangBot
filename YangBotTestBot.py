0faeb14b32e1ec622a986ff336ffb0a46cea9b7f
Dulaan <fu.dylan@gmail.com>
2021-07-01 15:21:58 -0700

    Separated test classes from bot.

--- YangBotTestBot.py
+++ YangBotTestBot.py
@@ -1,80 +1,28 @@
 import os
 from discord.ext import commands
-
+import YangBotTests
 my_secret = os.environ['TOKEN2']
 channel_id = int(os.environ['CHANNEL_ID'])
 bot = commands.Bot(command_prefix=';;')
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+  print(f'{bot.user.name} has connected to Discord!')
 
-
-class integration_test():
-    def __init__(self):
-        self.channel = bot.get_channel(channel_id)
-    async def __call__(self):
-        if self.message and self.check:
-            await self.channel.send(self.message)
-            try:
-                await bot.wait_for("message",check = self.check(),timeout = 15)
-                return True
-            except:
-                return False
-            
-class catfact(integration_test):
-# tests catfact
-    def __init__(self):
-        integration_test.__init__(self)
-        self.message = '$get_catfact'
-    def check(self):
-      def m(message):
-        return 'test' in message.content
-      return m
-
-class choose_empty(integration_test):
-# choose command unfilled
-  def __init__(self):
-    integration_test.__init__(self)
-    self.message = '$choose'
-  def check(self):
-    def m(message):
-      return 'Usage:' in message.content
-    return m
-
-class choose(integration_test):
-# choose command filled
-  def __init__(self):
-    integration_test.__init__(self)
-    self.message = '$choose x;x'
-  def check(self):
-    def m(message):
-      return 'x' in message.description # check this syntax for embeds
-    return m
-
-class toxicity_check(integration_test):
-  def __init__(self):
-    integration_test.__init__(self)
-    self.message = 'hey moron'
-def check(self):
-  def m(message):
-    return "Message has been marked for toxicity:" in message.embed.title and message.channel == 421899094357704704
-  return m 
 
 @bot.command(name = 'test')
-# command to run all integration tests
 async def test(m):
   testout = '```\nTest Results:\n'
   await m.send('testing...')
   
-  for i in integration_test.__subclasses__():
-    testing = i()
+  for i in YangBotTests.integration_test.__subclasses__():
+    channel = bot.get_channel(channel_id)
+    testing = i(bot,channel)
     check = await testing()
     if check == True:
-      testout+=('{}:\u2713 \n'.format(i().message))
+      testout+=('{}:\u2713 \n'.format(i.__name__))
     else:
-      testout+=('{}:\u2717 \n'.format(i().message))
+      testout+=('{}:\u2717 \n'.format(i.__name__))
   testout +='```'
   await m.send(testout)
-
 
 bot.run(my_secret)
---
+++ YangBotTests.py
@@ -0,0 +1,56 @@
+class integration_test():
+  async def __call__(self):
+      if self.message and self.check:
+        await self.channel.send(self.message)
+        try:
+          await self.bot.wait_for("message",check = self.check(),timeout = 15)
+          return True
+        except:
+         return False
+
+class catfact(integration_test):
+# tests catfact
+    def __init__(self,bot,channel):
+      integration_test.__init__(self)
+      self.bot = bot
+      self.channel = channel
+      self.message = '$get_catfact' # the command sent by the testbot
+    def check(self):
+      def m(message):
+        return 'unsubscribe' in message.content # returns T/F depending on Yangbot's return message
+      return m 
+
+class choose_empty(integration_test):
+# choose command unfilled
+  def __init__(self,bot,channel):
+    integration_test.__init__(self)
+    self.bot = bot
+    self.channel = channel
+    self.message = '$choose'
+  def check(self):
+    def m(message):
+      return 'Usage:' in message.content
+    return m
+
+class choose_filled(integration_test):
+# test choose command filled
+  def __init__(self,bot,channel):
+    integration_test.__init__(self)
+    self.bot = bot
+    self.channel = channel
+    self.message = '$choose x;x'
+  def check(self):
+    def m(message):
+      return 'x' in message.embed.description
+    return m
+
+class toxicity_check(integration_test):
+  def __init__(self,bot,channel):
+    integration_test.__init__(self)
+    self.bot = bot
+    self.channel = channel
+    self.message = 'hey moron'
+  def check(self):
+    def m(message):
+      return "Message has been marked for toxicity:" in message.embed.title and message.channel == 421899094357704704
+    return m
