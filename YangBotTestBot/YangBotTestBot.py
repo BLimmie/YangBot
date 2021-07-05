2146741956468129ed9fb8e3e02980b996fd31ce
Dulaan <fu.dylan@gmail.com>
2021-07-05 13:44:12 -0700

    Update YangBotTestBot.py
    Added on_ready() and YangBot status check

--- YangBotTestBot/YangBotTestBot.py
+++ YangBotTestBot/YangBotTestBot.py
@@ -1,19 +1,25 @@
 import os
 from discord.ext import commands
 import YangBotTests
+import asyncio
+import discord
 my_secret = os.environ['TOKEN2']
 channel_id = int(os.environ['CHANNEL_ID'])
 bot = commands.Bot(command_prefix=';;')
+
+
 @bot.event
-async def on_ready():
-  print(f'{bot.user.name} has connected to Discord!')
-
-
-@bot.command(name = 'test')
-async def test(m):
+async def on_ready(m):
+  yangBot = bot.get_user_info(392801609362440198)
+  timeout = 0
+  while yangBot.status != discord.Status.online:
+    if timeout < 5:
+      await asyncio.sleep(5)
+      timeout += 1
+    else:
+      return
   testout = '```\nTest Results:\n'
   await m.send('testing...')
-  
   for i in YangBotTests.integration_test.__subclasses__():
     channel = bot.get_channel(channel_id)
     testing = i(bot,channel)
