4aabf8b452ca8784500cd1bde3eea0f91a6fb7aa
Dulaan <fu.dylan@gmail.com>
2021-07-07 18:14:39 -0700

    Created func_decorator to wrap functions as funcblocker objects

--- commands_refactor.py
+++ commands_refactor.py
@@ -1,13 +1,11 @@
-# add funcblocker stuff later
+
 import psycopg2
 import random
-
+from new_funcblocker import func_decorator
 from src.tools.message_return import message_data
 from src.modules.db_helper import member_exists, insert_member, refresh_member_in_db
 from src.modules.discord_helper import change_nickname, kick_member, try_send
 from src.modules.catfact_helper import get_catfact
-
-# add funcblocker stuff later
 
 class bot_function:
   def __init__(self, bot):
@@ -26,27 +24,27 @@ class command_on_message(bot_function):
     return await self.action(message)
   def action(self,message):
     raise NotImplementedError
-
+@func_decorator()
 class catfact(command_on_message):
   async def action(self,message):
     return message_data(message.channel, get_catfact())
 
-
+@func_decorator()
 class register(command_on_message):
   async def action(self,message):
     print("Registering")
     user = message.author
-    conn = self.bot.conn
+    conn = bot.conn
     if not member_exists(conn, user.id):
-      insert_member(conn, self.bot, user)
+      insert_member(conn, bot, user)
     else:
       return message_data(message.channel, "User already registered")
     return message_data(message.channel, "User registered")
-
+@func_decorator()
 class resetregister(command_on_message):
   async def action(self, message):
     user = message.author
-    conn = self.bot.conn
+    conn = bot.conn
     if not member_exists(conn, user.id):
       return message_data(message.channel, "User not registered. Use $register to register.")
     try:
@@ -55,18 +53,18 @@ class resetregister(command_on_message):
       conn.commit()
     except psycopg2.Error as e:
       conn.rollback()
-    insert_member(conn, self.bot, user)
+    insert_member(conn, bot, user)
     return message_data(message.channel, "User registration reset")
-
+@func_decorator()
 class kickme(command_on_message):
   async def action(self, message):
-    conn = self.bot.conn
+    conn = bot.conn
     if not member_exists(conn, message.author.id):
       return message_data(message.channel, "You aren't registered in my memory yet. Please register with $register first")
     await message.author.send("See you later!")
     await kick_member(message.author)
     return
-
+@func_decorator()
 class nickname(command_on_message):
   async def nickname_request(self, message, member, new_nickname):
       if new_nickname == None:
@@ -76,7 +74,7 @@ class nickname(command_on_message):
       await message.add_reaction('❌')
       def check(reaction, user):
             return reaction.message.id == message.id and not user.bot and (str(reaction.emoji) == '✅' or str(reaction.emoji) == '❌')
-      reaction, user = await self.bot.client.wait_for("reaction_add", check=check)
+      reaction, user = await bot.client.wait_for("reaction_add", check=check)
       if str(reaction.emoji) == '✅':
         try:
           await change_nickname(member, new_nickname)
@@ -86,20 +84,18 @@ class nickname(command_on_message):
         await try_send(member, "Your nickname request has been approved")
       else:
         await try_send(member, "Your nickname request has been rejected")
-  
+
   async def action(self, message):
     user = message.author
     content = message.content
     if len(content.split()) < 2:
-      message.channel.send("No nickname requested, usage is $nickname [new nickname]")
-      return 
+      return message_data(message.channel,"No nickname requested, usage is $nickname [new nickname]")
     nickname = " ".join(content.split()[1:])
     if len(nickname) > 32:
-      message.channel.send("Nickname requested is too long")
-      return
-    requests_channel = self.bot.client.get_channel(bot.config["requests_channel"])
+      return message_data(message.channel,"Nickname requested is too long")
+    requests_channel = bot.client.get_channel(bot.config["requests_channel"])
     message = await requests_channel.send("Member {} is requesting a nickname change\nNew nickname: {}".format(user.display_name, nickname))
-    await self.nickname_request(message, user, nickname)
+    await nickname_request(message, user, nickname)
     return 
 
 
@@ -113,7 +109,7 @@ send_roles = [
   bot.config["roles"]["Yangbot Devs"],
   bot.config["roles"]["Server Legacy"]]
 
-
+@func_decorator(roles = send_roles)
 class send(command_on_message):
   async def action(self, message):
     content = message.content
@@ -143,3 +139,4 @@ class choose(command_on_message):
       "description": chosen_opt,
       "color": 53380}
       )
+    
