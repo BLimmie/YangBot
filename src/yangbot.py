9630462a23ad10a1a19f0bac86f9c7da735dfd6a
Dulaan <fu.dylan@gmail.com>
2021-07-12 16:29:15 -0700

    added auto_on_message functionality

--- src/yangbot.py
+++ src/yangbot.py
@@ -1,18 +1,34 @@
 import traceback
 from src.tools.funcblocker import funcblocker
 from commands_refactor import command_on_message
-
+from auto_on_message_refactor import auto_on_message
 
 
 class YangBot():
   def __init__(self,bot):
     self.bot = bot
     self.command_on_message_list = {}
+    self.auto_on_message_list = {}
+    
     for action in command_on_message.__subclasses__():
       action.bot = bot
       self.command_on_message_list[action.__name__] = action()
-      print(self.command_on_message_list[action.__name__].__dict__)
+    
+    for action in auto_on_message.__subclasses__():
+        action.bot = bot
+        self.auto_on_message_list[action.__name__] = action()
+  
   async def run_command_on_message(self,message):
     command = message.content.split()[0][1:]
     if command in self.command_on_message_list:
       return await self.command_on_message_list[command].proc(message,message.created_at,message.author)
+  
+  async def run_auto_on_message(self,message):
+      if message is not None:
+          for key, func in self.auto_on_message_list.items():
+              try:
+                  return await func.proc(message,message.created_at,message.author)
+              except Exception:
+                  traceback.print_exc()
+                  assert False
+                  
