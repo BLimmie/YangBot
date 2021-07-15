6b2ccd7058434e31289faa186807df83acf86c2c
Sharon Nguyen <sharonnguyen34@gmail.com>
2021-07-15 14:43:31 -0700

    Update member_join.py with bot_function

--- src/member_join.py
+++ src/member_join.py
@@ -1,10 +1,22 @@
 from src.tools.message_return import message_data
-
 from src.modules.db_helper import member_exists, fetch_member_roles, fetch_member_nickname
 from src.modules.discord_helper import add_roles, change_nickname
+from src.tools.bot_function import bot_function
 
+class on_member_join(bot_function):
+    registry = []
+    def __init__(self):
+        on_member_join.registery.append(self)
+    async def action(self, message):
+        raise NotImplementedError
 
-def init(bot):
+class restore_roles(on_member_join):
+    """
+    Restores roles and nickname on member join if member exists in database
+    """
+    def __init__(self):
+        super().__init__()
+        
     async def restore(message, user, member_roles, nickname):
         try:
             await add_roles(user, member_roles)
@@ -12,16 +24,12 @@ def init(bot):
             print("Roles cannot be added to user: {}".format(user.name))
         await change_nickname(user, nickname)
 
-    @bot.on_member_join(restore)
-    def restore_roles(user):
-        """
-        Restores roles and nickname on member join if member exists in database
-        """
-        conn = bot.conn
+    async def action(self, user):
+        conn = self.bot.conn
         if member_exists(conn, user.id):
-            roles = sorted(bot.config["role_schema"])
+            roles = sorted(self.bot.config["role_schema"])
             member = fetch_member_roles(conn, user.id, roles)
-            member_roles = [bot.client.get_guild(int(bot.config["server_id"])).get_role(
+            member_roles = [self.bot.client.get_guild(int(self.bot.config["server_id"])).get_role(
                 role) for role in member]
             nickname = fetch_member_nickname(conn, user.id)
             return message_data(user, "Your roles have been restored", args=[user, member_roles, nickname])
