12722f804e0e024e503d7c11b30d2ecb8f7f41d4
Sharon Nguyen <sharonnguyen34@gmail.com>
2021-07-15 14:05:03 -0700

    Update member_update.py with bot_function

--- src/member_update.py
+++ src/member_update.py
@@ -1,22 +1,27 @@
 import psycopg2
-
 from src.modules.db_helper import member_exists, insert_member
+from src.tools.bot_function import bot_function
 
+class on_member_update(bot_function):
+    registry = []
+    def __init__(self):
+        on_member_update.registery.append(self)
+    async def action(self, message):
+        raise NotImplementedError
 
-def init(bot):
-    @bot.on_member_update()
-    def update_database_roles(before, after):
-        """
-        Updates the member roles in the database after member is updated
-        """
+class update_database_roles(on_member_update):
+    """
+    Updates the member roles in the database after member is updated
+    """
+    async def action(self, before, after):
         user_id = after.id
-        conn = bot.conn
+        conn = self.bot.conn
         roles_deleted = [role.id for role in before.roles if role not in after.roles]
         roles_added = [role.id for role in after.roles if role not in before.roles]
         if len(roles_deleted) == 0 and len(roles_added) == 0:
             return
         if not member_exists(conn, user_id):
-            insert_member(conn, bot, after)
+            insert_member(conn, self.bot, after)
         for role in roles_deleted:
             try:
                 cur = conn.cursor()
@@ -43,16 +48,16 @@ def init(bot):
             except psycopg2.Error as e:
                 conn.rollback()
 
-    @bot.on_member_update()
-    def update_database_name(before, after):
-        """
-        Updates the member nickname in the database after member is updated
-        """
+class update_database_name(on_member_update):
+    """
+    Updates the member nickname in the database after member is updated
+    """
+    async def action(self, before, after):
         if before.display_name == after.display_name:
             return
-        conn = bot.conn
+        conn = self.bot.conn
         if not member_exists(conn, after.id):
-            insert_member(conn, bot, after)
+            insert_member(conn, self.bot, after)
         else:
             try:
                 cur = conn.cursor()
