3fb1473a69b888a2d8c1b0cb0e8278a97dbc170d
Sharon Nguyen <sharonnguyen34@gmail.com>
2021-07-15 16:52:41 -0700

    Spacing fix and debug to read a specific bot

--- run_refactor.py
+++ run_refactor.py
@@ -44,12 +44,12 @@ async def on_message(message):
         await Botter.run_auto_on_message(message)
     else:
         # Commands
-        if not message.author.bot:
+        if not message.author.bot or message.author.id == 856999058709938177:
             if message.content.startswith('$'):
                 return_message = await Botter.run_command_on_message(message)
-            if return_message is not None:
-                channel = return_message.channel
-                await channel.send(return_message.message, embed=return_message.embed)
+                if return_message is not None:
+                    channel = return_message.channel
+                    await channel.send(return_message.message, embed=return_message.embed)
             await Botter.run_auto_on_message(message)
 
 bot.run(my_secret)
