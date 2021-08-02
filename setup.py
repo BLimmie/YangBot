19d53939431b74decc44341b75c7a0b0ea58457c
Sharon Nguyen <sharonnguyen34@gmail.com>
2021-08-02 14:28:31 -0700

    Changed database setup for single varchar roles

--- setup.py
+++ setup.py
@@ -1,31 +1,19 @@
 import os
 import psycopg2
-import json
 
-config = json.load(open('config.json'))
-
-roles = config["roles"]
 DATABASE_URL = os.environ['YANGBOT_DB']
-
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
 cur = conn.cursor()
 
 cur.execute("""Drop table Members""")
 
 cur.execute("""
     CREATE TABLE Members (
-        id VARCHAR(20) PRIMARY KEY,
-        default_nickname TEXT NOT NULL
+        ID bigint PRIMARY KEY NOT NULL,
+        nickname VARCHAR(32) NOT NULL,
+        roles VARCHAR(255) NOT NULL
     );
     """)
-
-conn.commit()
-
-for role in sorted(roles.values()):
-    cur.execute("""
-    ALTER TABLE Members
-    ADD COLUMN role_%s bool DEFAULT False;
-    """,
-                (role,))
 
 conn.commit()
