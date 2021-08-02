69f4c2ca58003851e720c0b174db5289e7979f7f
Dulaan <fu.dylan@gmail.com>
2021-08-02 13:55:04 -0700

    extended yangbot integration tests

--- YangBotTestBot/YangBotTests.py
+++ YangBotTestBot/YangBotTests.py
@@ -1,3 +1,6 @@
+from src.commands_refactor import register
+
+
 class integration_test():
   async def __call__(self):
       if self.message and self.check:
@@ -29,7 +32,6 @@ class choose_empty(integration_test):
     self.message = '$choose'
   def check(self):
     def m(message):
-      print(message.content)
       return 'Usage:' in message.content
     return m
 
@@ -55,5 +57,98 @@ class toxicity_check(integration_test):
   def check(self):
     def m(message):
       embed = message.embeds[0]
-      return "Message has been marked for toxicity:" in embed.title and message.channel == 421899094357704704
+      return "Message has been marked for toxicity:" in embed.title and message.channel == 421899094357704704 # looks for message in toxicity check channel
     return m
+  
+class register_new(integration_test):
+  def __init__(self,bot,channel):
+    super().__init__()
+    self.bot = bot
+    self.channel = channel
+    self.message = '$register'
+  def check(self):
+    def m(message):
+      return 'User has been registered' in message.content
+    return m
+
+class register_existing(integration_test):
+  def __init__(self,bot,channel):
+    super().__init__()
+    self.bot = bot
+    self.channel = channel
+    self.message = '$register'
+  def check(self):
+    def m(message):
+      return 'User already registered' in message.content
+    return m
+
+class kickme_registered(integration_test):
+  def __init__(self,bot,channel):
+    self.bot = bot
+    self.channel = channel
+    self.message = '$kickme'
+  def check(self):
+    def m(message):
+      return 'See you later!' in message.content
+    return m
+
+class resetregister_existing(integration_test):
+  def __init__(self,bot,channel):
+    self.bot = bot
+    self.channel = channel
+    self.message = '$resetregister'
+  def check(self):
+    def m(message):
+      return 'User registration reset' in message.content
+    return m
+
+class resetregister_empty(integration_test):
+  def __init__(self,bot,channel):
+    self.bot = bot
+    self.channel = channel
+    self.message = '$resetregister'
+  def check(self):
+    def m(message):
+      return 'User not registered. Use $register to register.' in message.content
+    return m
+
+class kickme_unregistered(integration_test):
+  def __init__(self,bot,channel):
+    self.bot = bot
+    self.channel = channel
+    self.message = '$kickme'
+  def check(self):
+    def m(message):
+      return "You aren't registered in my memory yet. Please register with $register first" in message.content
+    return m
+
+
+class nickname_empty(integration_test):
+  def __init__(self,bot,channel):
+    self.bot = bot
+    self.channel = channel
+    self.message = '$nickname'
+  def check(self):
+    def m(message):
+      return "No nickname requested, usage is $nickname [new nickname]" in message.content
+    return m
+
+class nickname_too_long(integration_test):
+  def __init__(self,bot,channel):
+    self.bot = bot
+    self.channel = channel
+    self.message = '$nickname abcdefghijklmnopqrstuvwxyz12345678'
+  def check(self):
+    def m(message):
+      return 'Nickname requested is too long' in message.content and message.channel == 531336865886765057
+    return m
+
+class nickname(integration_test):
+  def __init__(self,bot,channel):
+    self.bot = bot
+    self.channel = channel
+    self.message = '$nickname test'
+    def check(self):
+      def m(message):
+        return "Your nickname request has been submitted" in message.content
+      return m
