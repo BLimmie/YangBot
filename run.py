c918607b0229cc3aa912fe92a16f42c1166e1930
Sharon Nguyen <sharonnguyen34@gmail.com>
2021-08-11 14:44:35 -0700

    Added prefix '$' for commands

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import discord
 import json
 import os
+from src.modules.db_helper import all_members
 import psycopg2
 
 from src.yangbot import YangBot
@@ -11,24 +12,25 @@ config = json.load(open('config.json'))
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-client = discord.Client()
+intents = discord.Intents().all()
+client = discord.Client(prefix = '$', intents=intents)
 @client.event
 async def on_ready():
     global bot
     bot = YangBot(conn, client, config)
     print("Bot is ready")
 
-
 @client.event
 async def on_message(message):
 
     if not message.author.bot or message.author.id == 856999058709938177: # TestBot ID
-        if message.content.startswith('$'):            
-            return_message = await bot.run_command_on_message(message)
-            if return_message is not None:
-                channel = return_message.channel
-                await channel.send(return_message.message, embed=return_message.embed)
-        await bot.run_auto_on_message(message)
+        # Command on Message
+        return_message = await bot.run_command_on_message(message)
+        if return_message is not None:
+            channel = return_message.channel
+            await channel.send(return_message.message, embed=return_message.embed)
+    # Auto on Message        
+    await bot.run_auto_on_message(message)
 
 
 @client.event
