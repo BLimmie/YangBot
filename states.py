e522e50ef62827911e57517fba0c4b48187bb131
Lily Li <lilyli024@gmail.com>
2022-07-13 16:37:52 -0700

    first draft

---
+++ states.py
@@ -0,0 +1,27 @@
+import discord
+import json
+from src.tools.message_return import message_data
+from discord_ui import Button
+from src.modules.discord_helper import generate_embed
+
+class state():
+    def __init__(self):
+        dictionary = {
+            "title": "{title}",
+            "subtitle": "{subtitle}",
+            "button1" : Button(label="label1"),
+            "button2" : Button(label2="label1"),
+             "fields": [
+                {"name": "name1", "value": "value1"},
+                {"name": "name2", "value": "value2"},]
+        }
+        self.template = json.dumps(dictionary)
+
+    def fill_template(self, **kwargs):
+        filled_temp = json.loads(self.template).format(kwargs)
+        # instead here use generate_embed
+        return json.dumps(filled_temp)
+
+    def generate_embed(self, message, **kwargs):
+        return message_data(message.channel, discord.Embed(kwargs))
+
