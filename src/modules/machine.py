31d9088e19512adf823669ddd01534c5fc187adb
Lily Li <lilyli024@gmail.com>
2022-07-15 17:54:55 -0700

    changed update_state

--- src/modules/machine.py
+++ src/modules/machine.py
@@ -1,11 +1,12 @@
 import discord
 from states import state
-from actions import action
+from action import action
 from typing import List
 from copy import deepcopy
-from src.tools.message_return import message_data
+import discord.ext.commands
 from discord_ui import Button, Interaction
 from src.modules.discord_helper import generate_embed
+import json
 
 '''
 Our idea for Machine:
@@ -35,16 +36,23 @@ class machine:
         self.history = deepcopy(history)
         self.current_state = current_state
         self.update_state(current_state)
-        
-    async def update_state(self, user_action: state):
+
+    async def update_state(self, user_action: action, interaction: Interaction):
         """
          1. Call determine_next_state and update self.current_state
          2. Edit embed with new attributes
          3. update history
         """
-        # Do stuff to change the current state
+        # maybe use interaction_check
+
+        self.current_state = user_action.determine_next_state()
         self.history.append(user_action)
 
+        interaction.edit(embed=generate_embed(json.loads(self.current_state.template)), 
+        components=[self.current_state.json.loads(self.current_state.template)["buttons"]])
+    
+    client=discord.ext.commands.Bot()    
+    @client.listen()
     async def interaction_check(self, interaction: Interaction, message) -> bool:
         await message.user == message.author
 
