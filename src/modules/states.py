5b2b169cc55e96e550722bfff390c4cc5fb76c3f
Lily Li <lilyli024@gmail.com>
2022-07-15 17:50:54 -0700

    updated template
    Rebased 'state-machines' to be on track with master.

--- src/modules/states.py
+++ src/modules/states.py
@@ -1,4 +1,3 @@
-import discord
 import json
 from discord_ui import Button
 from discord_helper import generate_embed
@@ -8,19 +7,15 @@ Idea for State:
 
 State is a blueprint which can be read by Machine to update itself. It does not do much on its own. Action is responsible for generating new states to pass onto machine, while machine is responsible for updating itself depending on the action.
 '''
-
 class state:
     def __init__(self):
         dictionary = {
-            "embed": {
-                "title": "{title}",
-                "subtitle": "{subtitle}",
-                "fields": [
-                    {"name": "name1", "value": "value1"},
-                    {"name": "name2", "value": "value2"},]
-            },
-            "buttons": [],
-            "data": {}
+            "title": "{title}",
+            "subtitle": "{subtitle}",
+             "fields": [
+                {"name": "name1", "value": "value1"},
+                {"name": "name2", "value": "value2"},],
+            "buttons": []
         }
         self.template = json.dumps(dictionary)
 
