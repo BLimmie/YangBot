0d56caa7a676c7a91c1d1e24e6ccad787032e49b
Lily Li <lilyli024@gmail.com>
2022-07-15 17:50:54 -0700

    updated template

--- src/modules/states.py
+++ src/modules/states.py
@@ -1,34 +1,25 @@
-import discord
 import json
-from src.tools.message_return import message_data
-from discord_ui import Button
+import discord.ext.commands
 from src.modules.discord_helper import generate_embed
-
 '''
 Idea for State:
 
 State is a blueprint which can be read by Machine to update itself. It does not do much on its own. Action is responsible for generating new states to pass onto machine, while machine is responsible for updating itself depending on the action.
 '''
-
 class state:
     def __init__(self):
         dictionary = {
-            "embed": {
-                "title": "{title}",
-                "subtitle": "{subtitle}",
-                "fields": [
-                    {"name": "name1", "value": "value1"},
-                    {"name": "name2", "value": "value2"},]
-            },
-            "buttons": [],
-            "data": {}
+            "title": "{title}",
+            "subtitle": "{subtitle}",
+             "fields": [
+                {"name": "name1", "value": "value1"},
+                {"name": "name2", "value": "value2"},],
+            "buttons": []
         }
         self.template = json.dumps(dictionary)
 
-    def fill_template(self, **kwargs):
-        filled_temp = json.loads(self.template).format(kwargs)
-        # instead here use generate_embed
+    def fill_template(self, embed_dict):
+        embed = generate_embed(embed_dict)
+        filled_temp = json.loads(self.template).format(embed.__dict__)
         return json.dumps(filled_temp)
 
-    def generate_embed(self, message, **kwargs):
-        return message_data(message.channel, discord.Embed(kwargs))
