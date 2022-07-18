153a37f0eb65028a30ea4dee808b7c0211e87c4f
Dan Le Man <58541834+DanLeEpicMan@users.noreply.github.com>
2022-07-18 16:40:45 -0700

    Additional comments, made a Interaction mimic class

--- src/modules/machine.py
+++ src/modules/machine.py
@@ -1,4 +1,3 @@
-from multiprocessing import dummy
 import discord
 import discord_ui
 from discord.ext import commands
@@ -23,7 +22,7 @@ Machine serves as a gateway to the embed (i.e. it is the machine itself). Machin
 Our idea for implementation:
 
 Machine: Represents the embed to be interacted with. Machine comes built in with a history, data, and a list of states (not necessary?)
-Action: Represents some execution that happens when machine is interacted with (i.e. a button is pressed). Action is responsibile for determining next state, which can be updated via machine.update_state
+Action: Represents some execution that happens when machine is interacted with (i.e. a button is pressed). Action is responsibile for determining next state, which can be updated via machine.update_state()
 State: Represents the current state of the machine, i.e. a new embed and any related data. Actions manipulate (and create) states, which is passed onto a machine to update.
 
 Key takeaways:
@@ -32,6 +31,19 @@ Key takeaways:
 2. Machine represents the embed itself. Its sole responsibility is to update itself, track a history, and hold data.
 3. States are blueprints for what the machine should look like. It contains information about the embed and actions. (Should it contain new data too?)
 '''
+
+#This is mimics an interaction object in order to bypass all of the duck tests used below
+class interaction_mimic:
+    def __init__(self, owner = None) -> None:
+        '''
+        An object that mimics `discord_ui.ButtonInteraction` in order to bypass duck tests.
+
+        Accepts the following parameters, all of which are optional:
+        `owner`
+
+        See `discord_ui.ButtonInteraction` for proper documentation of attributes.
+        '''
+        self.author = owner
 
 class machine:
     def __init__(self):
@@ -47,24 +59,29 @@ class machine:
         
         @params
         `client`: The bot itself.
-        `ui_client`: A UI extension allowing parsing of interactions. Must be initialized as UI(client) (should every machine have its own, or is it better to have one for all?)
+        `ui_client`: A UI extension allowing parsing of interactions. Must be initialized as UI(client) (should every machine initialize its own, or is it better to have one made then passed to all?)
         `message`: The message that initialized the machine.
         `initial_state`: A state object that the machine should put itself into upon creation.
         `states` (Optional?): A list of states the machine may enter. Empty by default.
         `actions` (Optional?): A list of valid actions the user may perform. Empty by default.
         `history` (Optional): A history of previous actions taken. Empty by default.
         '''
+
+        # Initialize 'private' variables
         self = cls()
+        self._client = client
+        self._ui = ui_client
         self._owner = message.author
         self._message = await ui_client.send(message.channel, content = 'Initializing...')
+
+        # Initialize 'public' variables
         self.states = deepcopy(states)
         self.actions = deepcopy(actions)
         self.history = deepcopy(history)
-        
+
+        # Put the machine into its initial state
         self.current_state = None
-        dummy_object = object() # This is used to bypass the duck test used in interaction_check.
-        dummy_object.author = self._owner
-        await self.update_state(initial_state, dummy_object)
+        await self.update_state(initial_state, interaction_mimic(self._owner))
 
 <<<<<<< HEAD
     async def update_state(self, user_action: action, interaction: Interaction):
@@ -76,6 +93,10 @@ class machine:
 >>>>>>> a37d118 (Changed 'create' method to send a message, and updated 'update_state' a bit.)
         """
         Edits the machine to match the given state.
+
+        @params
+        `new_state`: The state the machine should update itself to be in.
+        `interaction`: The interaction object that prompted a change in state.
         """
         if not self.interaction_check(interaction.author): return
 
