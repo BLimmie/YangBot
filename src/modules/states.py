0008c2ecae3f1ef1917abb579520cf576c13c0d7
Lily Li <lilyli024@gmail.com>
2022-07-19 17:00:21 -0700

    now using dict instead of json objects

--- src/modules/states.py
+++ src/modules/states.py
@@ -1,25 +1,32 @@
-import json
-import discord.ext.commands
+from discord import Embed
 from src.modules.discord_helper import generate_embed
-'''
+from copy import deepcopy
+
+"""
 Idea for State:
 
 State is a blueprint which can be read by Machine to update itself. It does not do much on its own. Action is responsible for generating new states to pass onto machine, while machine is responsible for updating itself depending on the action.
-'''
+"""
 class state:
     def __init__(self):
-        dictionary = {
+        self.embed_info = {
             "title": "{title}",
             "subtitle": "{subtitle}",
-             "fields": [
-                {"name": "name1", "value": "value1"},
-                {"name": "name2", "value": "value2"},],
+             "fields": [],
             "buttons": []
         }
-        self.template = json.dumps(dictionary)
+        self.data = {}
 
-    def fill_template(self, embed_dict):
-        embed = generate_embed(embed_dict)
-        filled_temp = json.loads(self.template).format(embed.__dict__)
-        return json.dumps(filled_temp)
+    @property
+    def embed(self) -> Embed:
+        """
+        Returns discord.Embed object based off the state's embed_info function
+        """
+        return generate_embed(self.embed_info)
+
+    @classmethod
+    def from_dic(self, embed_dict: dict, data: dict = {}):
+        self.embed_info = deepcopy(self.embed().__dict__)
+        self.data = deepcopy(data)
+
 
