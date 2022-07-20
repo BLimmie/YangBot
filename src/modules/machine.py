8804e5627b63ba5b7c7f19bef09d01ae254d63f1
Dan Le Man <58541834+DanLeEpicMan@users.noreply.github.com>
2022-07-19 19:12:19 -0700

    Updated listener subclass

--- src/modules/machine.py
+++ src/modules/machine.py
@@ -1,5 +1,6 @@
 import discord
 import discord_ui
+import asyncio
 from discord_ui import Listener
 from discord.ext import commands
 from discord_helper import generate_embed
@@ -16,7 +17,7 @@ import json
 '''
 Our idea for implementation:
 
-Machine: Represents the embed to be interacted with. Machine comes built in with a history, data, and a list of states (not necessary?)
+Machine: Represents the embed to be interacted with. Machine comes built in with a history and data. Contains methods relevant to changing states.
 Action: Represents some execution that happens when machine is interacted with (i.e. a button is pressed). Action is responsibile for determining next state, which can be updated via machine.update_state()
 State: Represents the current state of the machine, i.e. a new embed and any related data. Actions manipulate (and create) states, which is passed onto a machine to update.
 
@@ -35,17 +36,39 @@ In order to call certain actions via buttons, we can approach this one of two wa
 '''
 
 #This is mimics an interaction object in order to bypass all of the duck tests used below
-class _interaction_mimic:
+class _InteractionMimic:
+    '''
+    An object that mimics `discord_ui.ButtonInteraction` in order to bypass duck tests.
+
+    Accepts the following parameters as keyword arguments, all of which are optional:
+    `author`
+
+    See `discord_ui.ButtonInteraction` for proper documentation of attributes.
+    '''
     def __init__(self, *, author = None) -> None:
-        '''
-        An object that mimics `discord_ui.ButtonInteraction` in order to bypass duck tests.
 
-        Accepts the following parameters as keyword arguments, all of which are optional:
-        `author`
-
-        See `discord_ui.ButtonInteraction` for proper documentation of attributes.
-        '''
         self.author = author
+
+class _IndividualListener(Listener):
+    '''
+    A Listener subclass that allows interaction with single buttons. Maybe this should be the action class??
+    
+    The alternative is to develop a general method (similar to `message_on_command`) that takes in context from all button clicks would need to developed.
+    '''
+
+    def __init__(self, id: str, callback, timeout=None, target_users=None) -> None:
+        '''
+        Initializes a Listener tied to a given button.
+
+        ### Parameters
+        `id`: The ID of a button to listen to.
+        `callback`: The coroutine to be executed when the button is pressed. Will be passed a `ButtonInteraction` object.
+        '''
+        super().__init__(timeout, target_users)
+
+        @Listener.button(id) # Is this valid?? Does the decorator need to be called within the class? Or is it fine to do it here?
+        async def execute(self, ctx):
+            await callback(ctx)
 
 class machine:
     def __init__(self):
@@ -80,7 +103,7 @@ class machine:
 
         # Put the machine into its initial state
         self.current_state = None
-        await self.update_state(initial_state, _interaction_mimic(owner=self._owner))
+        await self.update_state(initial_state, _InteractionMimic(author=self._owner))
 
 <<<<<<< HEAD
     async def update_state(self, user_action: action, interaction: Interaction):
