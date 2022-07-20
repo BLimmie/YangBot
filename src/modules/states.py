900d6a2d9db9bd020497161c48790ab23738a782
Dan Le Man <58541834+DanLeEpicMan@users.noreply.github.com>
2022-07-19 17:11:16 -0700

    Added some additional methods to state, and changed its structure. Removed any use of JSON objects, since we'll be using buttons

--- src/modules/states.py
+++ src/modules/states.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import json
 <<<<<<< HEAD
 from discord_ui import Button
@@ -7,23 +8,33 @@ from discord_helper import generate_embed
 import discord.ext.commands
 from src.modules.discord_helper import generate_embed
 >>>>>>> a37d118 (Changed 'create' method to send a message, and updated 'update_state' a bit.)
+=======
+import discord.ext.commands
+from src.modules.discord_helper import generate_embed
+from discord import Embed
+
+>>>>>>> 6b84dbc (Added some additional methods to state, and changed its structure. Removed any use of JSON objects, since we'll be using buttons)
 '''
 Idea for State:
 
 State is a blueprint which can be read by Machine to update itself. It does not do much on its own. Action is responsible for generating new states to pass onto machine, while machine is responsible for updating itself depending on the action.
 '''
+
 class state:
     def __init__(self):
-        dictionary = {
-            "title": "{title}",
-            "subtitle": "{subtitle}",
-             "fields": [
-                {"name": "name1", "value": "value1"},
-                {"name": "name2", "value": "value2"},],
-            "buttons": []
+        '''
+        Initializes a blank state i.e. all fields are present but are empty. Default color is white.
+        '''
+        self.embed_info = {
+            "title": "",
+            "subtitle": "",
+            "color": 16777215,
+            "fields": [],
+            "components": []
         }
-        self.template = json.dumps(dictionary)
+        self.data = {}
 
+<<<<<<< HEAD
 <<<<<<< HEAD
     def fill_template(self, **kwargs):
         filled_temp = json.loads(self.template).format(kwargs)
@@ -34,5 +45,34 @@ class state:
         embed = generate_embed(embed_dict)
         filled_temp = json.loads(self.template).format(embed.__dict__)
         return json.dumps(filled_temp)
+=======
+    @classmethod
+    def from_dic(cls, embed_dict: dict, data: dict = {}):
+        '''
+        Creates a state based on the given dictionaries. Performs a shallow copy on both `embed_dict` and `data`.
+
+        ### Parameters
+        `embed_dict`: A dictionary with a list of attributes. Every key pair is optional; any missing keys pairs will be given default values. See `discord.Embed`'s attributes for a list of valid keys.
+        `data` (Optional): A dictionary with all relevant data for the machine. Defaults to a blank dictionary (`{}`)
+        '''
+        self = cls()
+        for key, value in embed_dict.items():
+            if key in self.embed_info:
+                self.embed_info[key] = value
+            else:
+                print('Unknown key "' + str(key) + '" with value "' + str(value) + '" given while attempting to generate a state.')
+        
+        for key, value in data.items():
+            self.data[key] = value
+
+        return self
+
+    @property
+    def embed(self) -> Embed:
+        '''
+        Returns a `discord.Embed` object based off the state's `embed_info` attribute.
+        '''
+        return generate_embed(self.embed_info)
+>>>>>>> 6b84dbc (Added some additional methods to state, and changed its structure. Removed any use of JSON objects, since we'll be using buttons)
 
 >>>>>>> a37d118 (Changed 'create' method to send a message, and updated 'update_state' a bit.)
