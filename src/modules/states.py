fb692550558b428ef345281da4111aa707a3b7be
Dan Le Man <58541834+DanLeEpicMan@users.noreply.github.com>
2022-07-22 23:37:45 -0700

    Changed the behavior for states. Allows for dotted access to Embed components, keyed access to data components.

--- src/modules/states.py
+++ src/modules/states.py
@@ -6,23 +6,33 @@ from discord_helper import generate_embed
 
 class state:
     '''
-    An object representing a state for a machine. Behaves like a dictionary (see Behavior)
+    An object representing a state for a machine. Behaves like a dictionary for its data, like an object for its embed (see Behavior).
 
-    ### Attributes
+    ## Attributes
 
-    `embed_info`: A dictionary describing attributes for a discord.Embed object
-
-    `data`: Any other data relevant for the machine.
+    `embed_info`: A dictionary describing attributes for a `discord.Embed` object
 
     `embed`: A `discord.Embed` object created with `embed_info`.
-    
-    ### Behavior
 
-    This class behaves like a dictionary.
+    `buttons`: A list of `discord.ui.Button` objects.
 
-    For getting values, it will return `embed_info[key]`. If it doesn't exist, then `data[key]` will be returned instead. 
+    `data`: Any other data relevant for the machine.
     
-    For setting values, it will check if the key is present in `embed_info`. If it is, then `embed_info[key] = value`. Otherwise, `data[key] = value`.
+    ## Methods
+
+    `@classmethod from_dict`: Creates a state based on the given dictionaries. Performs a shallow copy on all passed parameters.
+
+    `@classmethod from_state`: Creates a new state object from another state. Performs a deepcopy.
+
+    ## Behavior
+
+    This class behaves like a dictionary and object, allowing for keyed and dotted access. 
+
+    `state_obj.key` (dotted access) is equivalent to `state_obj.embed_info[key]`. Setting values behaves similarly.
+    
+    `state_obj[key]` (keyed access) is equivalent to `state_obj.data[key]`. Setting values behaves similarly.
+
+    `item in state_obj` is equivalent to `item in state_obj.data`.
     '''
     def __init__(self):
         '''
@@ -38,32 +48,48 @@ class state:
         self.data = {}
 
     def __getitem__(self, key):
-        return self.embed_info[key] if key in self.embed_info else self.data[key]
+        return self.data[key]
 
     def __setitem__(self, key, value) -> None:
+        self.data[key] = value
+
+    def __getattr__(self, key):
         if key in self.embed_info:
-            self.embed_info[key] = value
+            return self.embed_info[key]
         else:
-            self.data[key] = value
+            raise AttributeError(key + " is not a valid Embed attribute.")
+
+    def __setattr__(self, name, value):
+        if name in {*self.__dict__.keys(), "embed_info", "buttons", "data"}:
+            super().__setattr__(name, value)
+        else:
+            if name in self.embed_info:
+                self.embed_info[name] = value
+            else:
+                raise AttributeError(name + " is not a valid Embed attribute.")
+    
+    def __contains__(self, item):
+        return item in self.data
 
     @classmethod
-    def from_dict(cls, embed_dict: dict,*, buttons: List[Button] = [], data: dict = {}):
+    def from_dict(cls, embed_dict: dict, *, buttons: List[Button] = [], data: dict = {}):
         '''
         Creates a state based on the given dictionaries. Performs a shallow copy on all passed parameters.
 
         ## Parameters
 
         `embed_dict`: A dictionary with a list of attributes. All keys are optional; any missing keys will be given default values. See attributes of `discord.Embed` for a list of valid key-value pairs.
+
         `buttons` (Optional): A list of Button objects. Empty by default.
+
         `data` (Optional): A dictionary with all relevant data for the machine. Empty by default.
         '''
         self = cls()
-        for key, value in embed_dict.items():
-            if key in self.embed_info:
-                self.embed_info[key] = value
-            else:
-                print('Unknown key "' + str(key) + '" with value "' + str(value) + '" given while attempting to generate a state.')
-        
+        self.embed_info = {
+            key: value
+            for key, value in embed_dict.items()
+            if key in self.embed_info
+        }
         self.data = data.copy()
         self.buttons = buttons.copy()
         return self
@@ -104,11 +130,11 @@ class state:
         
 if __name__ == "__main__":
     # To test: How to add buttons to message. Seems like it's as simple as modifying `message.components`.
-    embed = generate_embed({
-            "title": "hey",
-            "description": "yo",
-            "color": 16777215
-        })
-    new_state = state()
-    new_state.embed = 'test'
-    print(new_state.embed_info)
+    new_state = state.from_dict({
+        "title": "hey",
+        "color": 5
+    }, data={'first': 1})
+    new_state.buttons = ['yo']
+    print(new_state.buttons)
+    print(new_state['first'])
+
