f2b4edd66938a55e3583289e426e0bd487e4104c
Dan Le Man <58541834+DanLeEpicMan@users.noreply.github.com>
2022-07-25 16:50:35 -0700

    Finished actions and got it into a usable state.

--- src/modules/actions.py
+++ src/modules/actions.py
@@ -1,30 +1,43 @@
-from platform import machine
-from typing import List
-from states import state
+from types import coroutine
+from discord.ui import Button
+from discord import ButtonStyle, Emoji
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 6c4a0b7 (Included comments (really multi-line strings) detailing our ideas for implementation. See each file (machines, states, actions) for specific comments on those classes, and machines for a general comment about implementation)
-'''
-Idea for action:
+async def _DefaultCallback(machine, interaction): # Instead of a print statement, call machine.update_state and change the embed message to the content inside the print statement.
+    print('Button was pressed by', interaction.user.name, 'in channel', interaction.channel.name)
 
-Action represents a button and what will happen upon interaction. It is responsible for manipulating data and creating a new state. 
-It will then pass this state onto machine, which will update itself accordingly.
-'''
+class action(Button):
+    '''
+    An object representing a button within discord. Designed to work with the machine class.
 
-class action:
-<<<<<<< HEAD
-=======
-class action():
->>>>>>> e8055ad (Created initial state machine files)
-=======
->>>>>>> 6c4a0b7 (Included comments (really multi-line strings) detailing our ideas for implementation. See each file (machines, states, actions) for specific comments on those classes, and machines for a general comment about implementation)
-    def __init__(self, current_state: state):
-       self.current_state = current_state
+    ## Parameters
+    All of the following parameters (except machine) are keyword-only and optional. Unless otherwise specified, all optional parameters default to `None`.
 
-    def determine_next_state(self, states: List[state]) -> state:
-        if states.index(self.current_state) < len(states) -1:
-            return states[states.index(self.current_state) +1 ]
-        return self.state_choice
-        # asumming we only move forward. doesn't take buttons into account yet but we can point to prior states
+    `machine`: The machine object this button is attached to.
+
+    `callback`: The coroute that will be invoked when the button is pressed. The action's machine and a `discord.Interaction` object is passed onto this callback as parameters. Defaults to a print statement.
+
+    It is expected that `callback` will generate a state object and call `update_state` onto its passed machine.
+
+    `style`: The style for the button. Defaults to `discord.ButtonStyle.blurple`.
+
+    `label`: The label (text) of the button.
+
+    `emoji`: A `discord.Emoji` object, representing the emoji of the button.
+
+    `row`: The row the button should be placed in, must be between 0 and 4 (inclusive). If this isn't specified, then automatic ordering will be applied.
+
+    `url`: A string representing the url that this button should send to. Note that specifying this changes some functionality (see discord.py docs).
+
+    `disabled`: Whether the button should invoke `callback` whenever pressed. Defaults to `False`.
+
+    ## Attributes
+
+    `machine`: The machine tied to this action.
+    '''
+    def __init__(self, machine, *, callback = _DefaultCallback , style: ButtonStyle=ButtonStyle.blurple, label: str=None, emoji: Emoji=None, row: int=None, url: str=None, disabled: bool=False):
+        super().__init__(style=style, label=label, emoji=emoji, row=row, url=url, disabled=disabled)
+        self.machine = machine
+        self._callback = callback
+
+    async def callback(self, interaction):
+        await self._callback(self.machine, interaction)
